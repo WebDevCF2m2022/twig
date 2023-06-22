a24d219bfd41eb616b9a205af03c579106778048
mikhawa <michaeljpitz@gmail.com>
2023-06-22 09:50:06 +0200

    articles

--- controller/publicController.php
+++ controller/publicController.php
@@ -1,11 +1,18 @@
 <?php
 
 use MyModels\Manager\thesectionManager;
+use MyModels\Manager\thearticleManager;
 
 $thesectionManager = new thesectionManager($pdo);
+$thearticleManager = new thearticleManager($pdo);
 
 try {
     $thesection = $thesectionManager->SelectAllThesection();
+} catch (Exception $e) {
+    $error = $e->getMessage();
+}
+try{
+    $thearticle = $thearticleManager->thearticleSelectAll();
 } catch (Exception $e) {
     $error = $e->getMessage();
 }
@@ -13,5 +20,7 @@ try {
 
 // Path: controller\publicController.php
 echo $twig->render("public/public_homepage.html.twig", [
-    "mesSections" => $thesection
+    // passage des sections et des articles à la vue
+    "mesSections" => $thesection,
+    "mesarticles"  => $thearticle,
 ]);
