c81d0cf5402abd55b0b6dfaf86dbb0fc498b2f74
mikhawa <michaeljpitz@gmail.com>
2023-06-22 15:34:59 +0200

    slugs

--- controller/publicController.php
+++ controller/publicController.php
@@ -3,9 +3,13 @@
 
 # chemin vers le manager de thesection
 use MyModels\Manager\thesectionManager;
+# chemin vers le manager de thearticle
+use MyModels\Manager\thearticleManager;
 
 # instanciation du manager de thesection
 $thesectionManager = new thesectionManager($pdo);
+# instanciation du manager de thearticle
+$thearticleManager = new thearticleManager($pdo);
 
 try {
     # récupération de toutes les sections pour le menu
@@ -14,10 +18,24 @@ try {
     $error = $e->getMessage();
 }
 
+if(isset($_GET['SlugSection'])){
+    echo $_GET['SlugSection'];
+}elseif(isset($_GET['SlugArticle'])){
+    echo $_GET['SlugArticle'];
+}else {
+
+    try {
+        # récupération de tous les articles pour la page d'accueil
+        $thearticle = $thearticleManager->thearticleSelectAll();
+    } catch (Exception $e) {
+        $error = $e->getMessage();
+    }
 
 // affichage de la vue qui est de l'environnement twig avec la méthode render et le chemin (il va chercher dans le dossier view)
-echo $twig->render("public/public_homepage.html.twig", [
-    // transmission de données à la vue
-    "mesSections" => $thesection, // pour le menu
-    "base_url" => MY_URL, // pour les liens
-]);
+    echo $twig->render("public/public_homepage.html.twig", [
+        // transmission de données à la vue
+        "mesSections" => $thesection, // pour le menu
+        "mesArticles" => $thearticle, // articles pour la page d'accueil
+        "base_url" => MY_URL, // pour les liens
+    ]);
+}
